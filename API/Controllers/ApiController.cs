f62d44e748d6901c6dcc01ba983969ca94405b10
Kyle Adkins <kyle.m.adkins@gmail.com>
2023-10-02 16:36:13 -0400

    Move mediator to base API controller, refactor details endpoint

--- API/Controllers/ApiController.cs
+++ API/Controllers/ApiController.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ApiController : ControllerBase { }
+    public class ApiController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>()!;
+    }
 }
--- API/Controllers/EventsController.cs
+++ API/Controllers/EventsController.cs
@@ -1,29 +1,26 @@
 ﻿using Application.Events;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     public class EventsController : ApiController
     {
-        private readonly IMediator _mediator;
-
-        public EventsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet(Name = "GetEvents")]
         public async Task<ActionResult<List<Event>>> GetEvents()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}", Name = "GetEvent")]
         public async Task<ActionResult<Event>> GetEvent(Guid id)
         {
-            return Ok();
+            var evt = await Mediator.Send(new Details.Query { Id = id });
+            if (evt == null)
+            {
+                return NotFound();
+            }
+            return evt;
         }
     }
 }
